e92847937edc8495f2e59dada627677c5b5c27a0
Kenan <kenanshafizade85@gmail.com>
2024-07-26 18:53:15 +0400

    Task 5

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,18 @@
+function createInt8TypedArray(length, position, value) {
+  // Create a new ArrayBuffer of the given length
+  const buffer = new ArrayBuffer(length);
+  // Create a DataView to work with the buffer
+  const view = new DataView(buffer);
+
+  // Check if the position is within the bounds of the buffer
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  // Set the Int8 value at the specified position
+  view.setInt8(position, value);
+
+  return view;
+}
+
+export default createInt8TypedArray;
