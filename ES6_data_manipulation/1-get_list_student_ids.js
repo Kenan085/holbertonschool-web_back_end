c2d14bd3a7c9bb6ea7413a11e2b18214cbbe3642
Kenan <kenanshafizade85@gmail.com>
2024-07-26 18:24:57 +0400

    fixed bugs I hope.

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,11 +1,11 @@
 function getListStudentIds(students) {
-    // Check if the input is an array
-    if (!Array.isArray(students)) {
-        return [];
-    }
+  // Check if the input is an array
+  if (!Array.isArray(students)) {
+    return [];
+  }
 
-    // Use map to create an array of ids
-    return students.map((student) => (student.id));
+  // Use map to create an array of ids
+  return students.map((student) => (student.id));
 }
 
 export default getListStudentIds;
