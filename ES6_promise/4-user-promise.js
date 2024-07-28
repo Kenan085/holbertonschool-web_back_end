f02be00f1c5e07559e06c6cfb993a36e2efc743c
Kenan <kenanshafizade85@gmail.com>
2024-07-28 21:14:04 +0400

    Task 7-9 added, 3-6 fixed

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default function signUpUser(firstName, lastName) {
-    return Promise.resolve({
-        firstName,
-        lastName,
-    });
+  return Promise.resolve({
+    firstName,
+    lastName,
+  });
 }
