151eeaea06fb0c17c7f579ffae5e875040c5fbc9
Kenan <kenanshafizade85@gmail.com>
2024-09-22 19:45:18 +0000

    added

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,21 +1,11 @@
 const express = require('express');
 
-const app = express();
 const port = 1245;
+const app = express();
+module.exports = app;
 
-// Обработчик для корневого маршрута
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-// Обработка ошибок 404
-app.use((req, res) => {
-  res.status(404).send('Not Found');
-});
-
-// Запуск сервера
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-
-module.exports = app;
+app.listen(port);
